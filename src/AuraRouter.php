3bcbf6c8420ed520e389b37e99976372936675e4
oscarotero <oom@oscarotero.com>
2018-01-24 21:21:16 +0100

    Code style fix

--- src/AuraRouter.php
+++ src/AuraRouter.php
@@ -4,11 +4,11 @@ declare(strict_types = 1);
 namespace Middlewares;
 
 use Aura\Router\RouterContainer;
-use Psr\Http\Server\MiddlewareInterface;
-use Psr\Http\Server\RequestHandlerInterface;
 use Middlewares\Utils\Factory;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 
 class AuraRouter implements MiddlewareInterface
 {
